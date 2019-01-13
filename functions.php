fea3efce18368078e77e68f75a404b2c883bdbbe
Unknown <opticalfalicy@gmail.com>
2019-01-13 00:03:27 -0600

    Section 5-9

--- functions.php
+++ functions.php
@@ -8,5 +8,32 @@
     wp_enqueue_style('grocery_main_styles', get_stylesheet_uri(), NULL, microtime());
   }
 
+  function grocery_features() {
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuLocation1', 'Footer Menu Location 1');
+    register_nav_menu('footerMenuLocation2', 'Footer Menu Location 2');
+    add_theme_support('title-tag');
+  }
+
+  function grocery_adjust_queries($query){
+    if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()){
+      $today = date('Ymd');
+      $query->set('meta_key', 'event_date');
+      $query->set('orderby', 'meta_value_num');
+      $query->set('order', 'ASC');
+      $query->set('meta_query', array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'numeric'
+        )
+      ));
+    }
+  }
+
   add_action('wp_enqueue_scripts', 'grocery_files');
+  add_action( 'after_setup_theme', 'grocery_features');
+  add_action('pre_get_posts', 'grocery_adjust_queries');
+
 ?>
